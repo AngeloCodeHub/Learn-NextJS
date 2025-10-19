d3b3306ee495ae7f10482fb9e4ad7332cdef6cdb
Angelo <a0920217700@gmail.com>
2025-10-20 04:33:34 +0800

    Chapter 12 CRUD 操作

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,15 +7,16 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+            選擇客戶名稱
           </label>
           <div className="relative">
             <select
@@ -40,7 +41,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+            發票金額 (USD)
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
